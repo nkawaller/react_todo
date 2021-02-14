af801a22f5603997ec775c32ba57ae95af5d33f9
Nathan Kawaller <kawaller@gmail.com>
2021-02-14 13:31:20 -0500

    todo app

---
+++ src/Test.js
@@ -0,0 +1,49 @@
+
+import {useState} from 'react';
+import Header from './Components/Header';
+import Tasks from './Components/Tasks';
+
+function App() {
+    const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: "Doctor's Appointment",
+        day: "February 5th at 2:30pm",
+        reminder: true 
+    },
+    {
+        id: 2,
+        text: "Meeting at School",
+        day: "February 6th at 1:30pm",
+        reminder: true 
+    },
+    {
+        id: 3,
+        text: "Food Shopping",
+        day: "February 5th at 2:30pm",
+        reminder: false 
+    }
+]);
+
+// Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+// Toggle Reminder
+const toggleReminder = (id) => {
+  setTasks(tasks.map((task) => task.id === id 
+  ? { ...task, reminder: !task.reminder} : task))
+}
+
+  return (
+    <div className="container">
+        <Header />
+        {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask}
+        onToggle={toggleReminder}/> :(
+           'No tasks to show')}
+    </div>
+  );
+}
+
+export default App;
